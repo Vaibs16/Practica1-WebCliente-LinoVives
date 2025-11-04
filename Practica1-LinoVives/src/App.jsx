8a2a5c7c0a652a77386a4d14d64e354c6c5ced1d
Lino <linovivescolon@gmail.com>
2025-11-04 18:57:52 +0000

    Creación de componente Searchbar
    He hecho limpieza de los archivos que voy a utilizar, y he creado el componente SearchBar, que será la barra de búsqueda de  series. También he añadido un poco de css.

--- Practica1-LinoVives/src/App.jsx
+++ Practica1-LinoVives/src/App.jsx
@@ -1,35 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import SearchBar from './components/SearchBar'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  
+  const [search, setSearch] = useState('');
 
+  const handleSearch = (query) => {
+    setSearch(query);
+    console.log("Buscando:", query);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    <div>
+      <h1>Buscador de series</h1>
+
+      <div className="search-container">
+        <SearchBar onSearch = {handleSearch}></SearchBar>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
-export default App
---
+++ Practica1-LinoVives/src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import './SearchBar.css';
+
+export default function SearchBar( {onSearch} ) {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value)
+    };
+
+    const handleSummit = () => {
+        onSearch(inputValue);
+    };
+
+    return (
+        <form onSubmit={handleSummit} className="search-form">
+            <input className = "search-input" type = "text" value={inputValue} onChange={handleChange} placeholder="Introduzca serie..." />
+            <button type = "submit" className="search-button">Buscar</button>
+        </form>
+    );
+
+
+}
