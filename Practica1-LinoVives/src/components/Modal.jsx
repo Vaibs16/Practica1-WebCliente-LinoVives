fdb71e821517cd09e9a96b1b7f24ccbca9b7804d
Lino <linovivescolon@gmail.com>
2025-11-11 18:25:42 +0100

    Corrección BUG 3
    Otra corrección de error, en este caso cuando se abría el modal, dándole a cualquier parte de la pantalla se cerraba. Añadiendo stopPropagation solo se cierra al darle al botón de cerrar.

--- Practica1-LinoVives/src/components/Modal.jsx
+++ Practica1-LinoVives/src/components/Modal.jsx
@@ -3,8 +3,8 @@ import "./Modal.css";
 export default function Modal({ show, onClose, children }) {
   if (!show) return null;
 
-  return (
-    <div className="modal-overlay" onClick={onClose}>
+  return ( //El StopPropagation sirve para que el modal se cierre únicamente cuando se le da al botón de cerrar (X). Sin él, se cierra al darle a cualquier parte de la pantalla.
+    <div className="modal-overlay" onClick={(e) => e.stopPropagation()}> 
       <div className="modal-content">
         <button className="modal-close" onClick={onClose}>×</button>
         {children}
